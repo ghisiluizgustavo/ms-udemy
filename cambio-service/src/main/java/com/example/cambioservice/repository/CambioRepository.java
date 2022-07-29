fa079f3e98b5032594d71be377446b7c244b844c
Luiz Gustavo Ghisi <ghisiluizgustavo@gmail.com>
2022-07-28 21:32:52 -0300

    init

---
+++ cambio-service/src/main/java/com/example/cambioservice/repository/CambioRepository.java
@@ -0,0 +1,9 @@
+package com.example.cambioservice.repository;
+
+import com.example.cambioservice.model.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Cambio findByFromAndTo(String from, String to);
+}
